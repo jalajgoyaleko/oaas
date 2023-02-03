f85bb0bf6bc6ac0b1ffafd9e9fa2f8b3fc44b8f3
jalajgoyaleko <jalaj.goyal@eko.co.in>
2023-02-03 16:24:57 +0530

    package json changes

--- src/components/Common/Labelglobal.tsx
+++ src/components/Common/Labelglobal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type LabelProps = {
   className: string;
-  children?: React.ReactElement;
+  children?: JSX.Element | string;
 };
 const Labelglobal = ({ className, children }: LabelProps) => {
   return <label className={className}>{children}</label>;
