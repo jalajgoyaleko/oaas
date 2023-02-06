af30b48562756f16d78027e0401b0450c879dd6a
jalajgoyaleko <jalaj.goyal@eko.co.in>
2023-02-06 11:29:07 +0530

    package json changes and webpack changes

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,25 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        loader: 'babel-loader',
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: '/node_modules/'
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist')
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
